4ea3b800a34bda5188c4d9f86db3da1ce9b18bbb
luo <381219883@qq.com>
2020-07-15 10:34:16 +0800

    修改值显示

--- src/page/asset/assetRouter.jsx
+++ src/page/asset/assetRouter.jsx
@@ -1,77 +1,89 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Redirect, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Switch, Redirect, Route, Link } from 'react-router-dom';
+import Loadable from 'react-loadable';
+import loading from '../../util/loading.jsx';
 // 页面
-import assetList from './assetList.jsx';
-import assetEdit from './assetEdit.jsx';
-import gatewayList from './gatewayList.jsx';
-import gatewayEdit from './gatewayEdit.jsx';
-import assetmapGaoDe from './assetmapGaoDe.jsx';
-import gatewayManagementOld from './gatewayManagementOld.jsx';
+// import assetList from './assetList.jsx';
+// import assetEdit from './assetEdit.jsx';
+// import gatewayList from './gatewayList.jsx';
+// import gatewayEdit from './gatewayEdit.jsx';
+// import assetmapGaoDe from './assetmapGaoDe.jsx';
+// import gatewayManagementOld from './gatewayManagementOld.jsx';
 
-import dashboard from './dashboard.jsx'
-import assetAlarmList from './assetAlarmList.jsx';
-import gatewayMonitor from './gatewayMonitor.jsx';
-import assetInventory from './assetInventory.jsx';
-import assetCube from './assetCube.jsx';
-
-
+// import dashboard from './dashboard.jsx'
+// import assetAlarmList from './assetAlarmList.jsx';
+// import gatewayMonitor from './gatewayMonitor.jsx';
+// import assetInventory from './assetInventory.jsx';
+// import assetCube from './assetCube.jsx';
 
 
+const assetAlarmList = Loadable({
+    loader: () => import(/* webpackChunkName: "assetCube" */ './assetAlarmList.jsx'),
+    loading: loading,
+    delay: 3000
+});
 
-// const assetCube = Loadable({
-//     loader: () => import(/* webpackChunkName: "assetCube" */ './assetCube.jsx'),
-//     loading: loading,
-//     delay: 3000
-// });
-
-// const assetmapGaoDe = Loadable({
-//     loader: () => import(/* webpackChunkName: "assetmapGaoDe" */ './assetmapGaoDe.jsx'),
-//     loading: loading,
-//     delay: 3000
-// });
-
+const gatewayMonitor = Loadable({
+    loader: () => import(/* webpackChunkName: "assetmapGaoDe" */ './gatewayMonitor.jsx'),
+    loading: loading,
+    delay: 3000
+});
 
 
-// const assetList = Loadable({
-//     loader: () => import('./assetList.jsx'),
-//     loading: loading,
-//     delay: 3000
-// });
+const assetCube = Loadable({
+    loader: () => import(/* webpackChunkName: "assetCube" */ './assetCube.jsx'),
+    loading: loading,
+    delay: 3000
+});
 
-// const assetEdit = Loadable({
-//     loader: () => import('./assetEdit.jsx'),
-//     loading: loading,
-//     delay: 3000
-// });
+const assetmapGaoDe = Loadable({
+    loader: () => import(/* webpackChunkName: "assetmapGaoDe" */ './assetmapGaoDe.jsx'),
+    loading: loading,
+    delay: 3000
+});
 
-// const gatewayManagementOld = Loadable({
-//     loader: () => import(/* webpackChunkName: "gatewayManagementOld" */ './gatewayManagementOld.jsx'),
-//     loading: loading,
-//     delay: 3000
-// });
-// const assetInventory = Loadable({
-//     loader: () => import(/* webpackChunkName: "assetInventory" */ './assetInventory.jsx'),
-//     loading: loading,
-//     delay: 3000
-// });
 
-// const assetMonitoring = Loadable({
-//     loader: () => import(/* webpackChunkName: "assetMonitoring" */ './assetMonitoring.jsx'),
-//     loading: loading,
-//     delay: 3000
-// });
 
-// const gatewayManagement = Loadable({
-//     loader: () => import(/* webpackChunkName: "gatewayManagement" */ './gatewayManagement.jsx'),
-//     loading: loading,
-//     delay: 3000
-// });
+const assetList = Loadable({
+    loader: () => import('./assetList.jsx'),
+    loading: loading,
+    delay: 3000
+});
 
-// const addGateway = Loadable({
-//     loader: () => import(/* webpackChunkName: "addGateway" */ './addGateway.jsx'),
-//     loading: loading,
-//     delay: 3000
-// });
+const assetEdit = Loadable({
+    loader: () => import('./assetEdit.jsx'),
+    loading: loading,
+    delay: 3000
+});
+
+const gatewayManagementOld = Loadable({
+    loader: () => import(/* webpackChunkName: "gatewayManagementOld" */ './gatewayManagementOld.jsx'),
+    loading: loading,
+    delay: 3000
+});
+const assetInventory = Loadable({
+    loader: () => import(/* webpackChunkName: "assetInventory" */ './assetInventory.jsx'),
+    loading: loading,
+    delay: 3000
+});
+
+const gatewayList = Loadable({
+    loader: () => import(/* webpackChunkName: "assetMonitoring" */ './gatewayList.jsx'),
+    loading: loading,
+    delay: 3000
+});
+
+const dashboard = Loadable({
+    loader: () => import(/* webpackChunkName: "gatewayManagement" */ './dashboard.jsx'),
+    loading: loading,
+    delay: 3000
+});
+
+const gatewayEdit = Loadable({
+    loader: () => import(/* webpackChunkName: "addGateway" */ './gatewayEdit.jsx'),
+    loading: loading,
+    delay: 3000
+});
 
 
 
